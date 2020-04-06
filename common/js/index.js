773645b8c01f3d482ebe234e3bb17dd1e9aea83d
YUNSEONG HAN <gksdbstjd1525@gmail.com>
2020-04-06 14:31:55 +0900

    time setting update

--- common/js/index.js
+++ common/js/index.js
@@ -1,19 +1,29 @@
 const alwaysDisplayWrap = document.querySelector('.always-display');
 const koDay = ['일', '월', '화', '수', '목', '금', '토'];
+const date = new Date();
 
+// 전체 날짜, 시간 관련
+let timeSetting = {
+    default : {
+        month : date.getMonth() + 1,
+        ddate : date.getDate(),
+        day : date.getDay(),
+        koDay : koDay[date.getDay()],
+        hours : date.getHours(),
+        minutes : date.getMinutes()
+    },
+
+    alwaysDisplayTime : () => {
+        document.querySelector('#phone .always-display .time-box .time').innerHTML = timeSetting.default.hours + ':' + timeSetting.default.minutes;
+        document.querySelector('#phone .always-display .time-box .date').innerHTML = timeSetting.default.month + '월 ' + timeSetting.default.ddate + '일 ' + timeSetting.default.koDay + '요일';;
+    }
+}
+
+// AlwaysDisplay
 let alwaysDisplay = {
     data : {
         count : 0,
         interval : null
-    },
-
-    timeSetting : () => {
-        let date = new Date();
-        let timeText = date.getHours() + ':' + date.getMinutes();
-        let dateText = date.getMonth() + 1 + '월 ' + date.getDate() + '일 ' + koDay[date.getDay()] + '요일';
-
-        document.querySelector('#phone .always-display .time-box .time').innerHTML = timeText;
-        document.querySelector('#phone .always-display .time-box .date').innerHTML = dateText;
     },
 
     fingerDown : () => {
@@ -36,7 +46,7 @@ let alwaysDisplay = {
 }
 
 window.onload = () => {
-    alwaysDisplay.timeSetting();
+    timeSetting.alwaysDisplayTime()
 
     document.querySelector('.always-display .finger-print').addEventListener('mousedown', () => { alwaysDisplay.fingerDown() });
     document.querySelector('.always-display .finger-print').addEventListener('mouseup', () => { alwaysDisplay.fingerUp() });
